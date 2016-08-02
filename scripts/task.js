2b0e2d3527051a015af9728e841264e1c69e4536
tkhuynh <thequest2311@yahoo.com>
2016-08-02 00:11:46 -0700

    edit task then save in firebase

--- scripts/task.js
+++ scripts/task.js
@@ -1,13 +1,23 @@
 'use strict';
 
-app.controller('TaskController', function($scope, FURL, $firebase, $location) {
+app.controller('TaskController', function($scope, FURL, $firebase, $location, $routeParams) {
 	var ref = new Firebase(FURL);
 	var fbTasks = $firebase(ref.child('task')).$asArray();
+	var taskId = $routeParams.taskId;
 
 	$scope.tasks = fbTasks;
 
 	$scope.postTask = function(task) {
 		fbTasks.$add(task);
+		$location.path('/browse');
+	};
+
+	if (taskId) {
+		$scope.selectedTask = $firebase(ref.child('task').child(taskId)).$asObject();
+	}
+
+	$scope.updateTask = function(task) {
+		$scope.selectedTask.$save(task);
 		$location.path('/browse');
 	};
 });
